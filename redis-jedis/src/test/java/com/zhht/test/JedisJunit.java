49939a96698c530688c679cc3181685125accf0d
whatislife <1053400192@qq.com>
2020-03-08 23:02:24 +0800

    jedis sentinel部分代码提交

--- redis-jedis/src/test/java/com/zhht/test/JedisJunit.java
+++ redis-jedis/src/test/java/com/zhht/test/JedisJunit.java
@@ -3,6 +3,8 @@ package com.zhht.test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import redis.clients.jedis.Jedis;
 /**
@@ -14,6 +16,18 @@ import redis.clients.jedis.Jedis;
 *
  */
 public class JedisJunit {
+	private Logger logger = LoggerFactory.getLogger(JedisJunit.class);
+	/**
+	 * 
+	* <p>Title: initLogger  </p>
+	* Description: 日志文件添加 parent中日志和实现 
+	* @author  frankman
+	* @date 2020年3月8日 下午7:47:17
+	 */
+	@Test
+	public void initLogger(){
+		logger.info("{} is {}", "logger","完美");
+	}
 	/*
 	 * @Before public void setUp() throws Exception {
 	 * System.out.println("setUp()"); //两个Before }
